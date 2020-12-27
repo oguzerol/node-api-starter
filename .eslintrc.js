0d61cf63c605028e8ef98b2c9b5489e1e582a1a3
oguzerol <oguzerol91@gmail.com>
2020-12-27 23:07:03 +0300

    Add knex.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,27 @@
 module.exports = {
-    env: {
-        jest: true,
-    },
-    extends: 'airbnb-base',
-    rules: {
-        'comma-dangle': 0,
-        'no-underscore-dangle': 0,
-        'no-param-reassign': 0,
-        'no-return-assign': 0,
-        camelcase: 0,
-    }
+  root: true,
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  ignorePatterns: ["node_modules/**/*.js"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    camelcase: 0,
+    "no-param-reassign": 0,
+    "no-return-assign": 0,
+    "no-underscore-dangle": 0,
+    "no-plusplus": 0,
+  },
 };
---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:10-alpine
+
+# Create app directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+RUN yarn add node-gyp 
+RUN npm install -g nodemon
+RUN yarn add bcrypt
+
+# Install app dependencies
+COPY package*.json /usr/src/app/
+RUN yarn install
+
+# Bundle app source
+COPY . /usr/src/app
+
+EXPOSE 5000
+
+CMD [ "yarn", "start" ]
